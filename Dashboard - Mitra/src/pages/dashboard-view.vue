829e343703c5724b2f33c07cf3c4d689cbd9da62
Ivan Christian <120652762+christvann@users.noreply.github.com>
2024-08-17 12:09:02 +0700

    Add files via upload

---
+++ Dashboard - Mitra/src/pages/dashboard-view.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex">
+    <SidebarKemitraan/>
+    <div class="flex-grow"> 
+      <MainBar/> 
+      <DashBoardMitra/>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+
+import MainBar from '@/components/MainBar.vue';
+import SidebarKemitraan from '@/components/SidebarKemitraan.vue';
+import DashBoardMitra from '@/components/DashBoardMitra.vue';
+
+
+export default {
+  components: {
+    SidebarKemitraan,
+    MainBar,
+    DashBoardMitra,
+  }
+};
+</script>

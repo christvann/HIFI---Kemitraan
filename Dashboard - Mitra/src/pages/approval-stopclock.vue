59b615784e7f376ebb52d5ba75167bc5480b79d1
RizkySetyoWicaksono <114576773+RizkySetyoWicaksono@users.noreply.github.com>
2024-08-26 15:56:12 +0700

    Add files via upload

---
+++ Dashboard - Mitra/src/pages/approval-stopclock.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="">
+      <div class="background">
+        <div class="flex">
+          <SidebarKemitraan/>
+          <div class="flex-grow"> 
+            <MainBar/> 
+            <ApprovalStopclock/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  import MainBar from '@/components/MainBar.vue';
+  import SidebarKemitraan from '@/components/SidebarKemitraan.vue';
+  import ApprovalStopclock from '@/components/ApprovalStopclock.vue';
+  
+  export default {
+    components: {
+      SidebarKemitraan,
+      ApprovalStopclock,
+      MainBar,
+    }
+  };
+  </script>
+  
+  <style>
+  .background {
+    width: Fixed (1,366px)px;
+    height: Hug (1,416px)px;
+    left: 3733px;
+    gap: 0px;
+    opacity: 0px;
+    background: #F6F7FD;
+  }
+  </style>
+  
---
+++ Dashboard - Mitra/src/pages/selesai-ditolak.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="">
+      <div class="background">
+        <div class="flex">
+          <SidebarKemitraan/>
+          <div class="flex-grow"> 
+            <MainBar/> 
+            <SelesaiDitolak/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script>
+
+import MainBar from '@/components/MainBar.vue';
+import SidebarKemitraan from '@/components/SidebarKemitraan.vue';
+import SelesaiDitolak from '@/components/SelesaiDitolak.vue';
+
+export default {
+    components: {
+    SidebarKemitraan,
+    MainBar,
+    SelesaiDitolak,
+    }
+};
+</script>
+
+<style>
+.background {
+  width: Fixed (1,366px)px;
+  height: Hug (1,416px)px;
+  left: 3733px;
+  gap: 0px;
+  opacity: 0px;
+  background: #F6F7FD;
+}
+</style>

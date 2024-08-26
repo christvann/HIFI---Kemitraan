34e3fde9b950506a9254e5122e6ab9118e481de2
RizkySetyoWicaksono <114576773+RizkySetyoWicaksono@users.noreply.github.com>
2024-08-26 15:54:44 +0700

    Add files via upload

--- Dashboard - Mitra/src/router/index.js
+++ Dashboard - Mitra/src/router/index.js
@@ -2,6 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import DashboardView from '../pages/dashboard-view.vue';
 import MasukManager from '../pages/masuk-manager.vue';
 import DetailPengajuanManager from '../pages/detail-pengajuan-manager.vue';
+import ApprovalMitra from '../pages/approval-view.vue';
+import ApprovalStopclock from '../pages/approval-stopclock.vue';
+import DetailPengajuanApprovalStopclock from '../pages/detail-pengajuan-approval-stopclock.vue';
+import DetailPengajuanApproval from '../pages/detail-pengajuan-approval.vue';
+import ProsesiMitra from '../pages/proses-view.vue';
+import SelesaiMitra from '../pages/selesai-view.vue';
+import SelesaiDitolak from '../pages/selesai-ditolak.vue';
 
 const routes = [
   {
@@ -19,6 +26,42 @@ const routes = [
     path: '/masukmanager/detailpengajuanmanager',
     component: DetailPengajuanManager
   },
+  {
+    name: 'ApprovalMitra',
+    path: '/approval',
+    component: ApprovalMitra
+  },
+  {
+    name: 'ApprovalStopclock',
+    path: '/approval/approvalstopclock',
+    component: ApprovalStopclock
+  },
+  {
+    name: 'DetailPengajuanApproval',
+    path: '/approval/detailpengajuanapproval',
+    component: DetailPengajuanApproval
+  },
+  {
+    name: 'DetailPengajuanApprovalStopclock',
+    path: '/approval/approvalstopclock/detailpengajuanapprovalstopclock',
+    component: DetailPengajuanApprovalStopclock
+  },
+  {
+    name: 'ProsesiMitra',
+    path: '/proses',
+    component: ProsesiMitra
+  },
+  {
+    name: 'SelesaiMitra',
+    path: '/selesai',
+    component: SelesaiMitra
+  },
+  {
+    name: 'Ditolak',
+    path: '/selesai/ditolak',
+    component: SelesaiDitolak
+  }
+
 ];
 
 function Router() {

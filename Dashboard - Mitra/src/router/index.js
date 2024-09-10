88930ed40e9df5be389338db70063ac104b017f1
Ivan Christian <120652762+christvann@users.noreply.github.com>
2024-09-10 11:15:24 +0700

    Add files via upload

--- Dashboard - Mitra/src/router/index.js
+++ Dashboard - Mitra/src/router/index.js
@@ -1,16 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '@/pages/login-page.vue';
+import HomePage from '@/pages/home-page.vue';
 import DashboardView from '../pages/dashboard-view.vue';
-import MasukManager from '../pages/masuk-manager.vue';
-import DetailPengajuanManager from '../pages/detail-pengajuan-manager.vue';
-import ApprovalMitra from '../pages/approval-view.vue';
-import ApprovalStopclock from '../pages/approval-stopclock.vue';
-import DetailPengajuanApprovalStopclock from '../pages/detail-pengajuan-approval-stopclock.vue';
-import DetailPengajuanApproval from '../pages/detail-pengajuan-approval.vue';
-import ProsesiMitra from '../pages/proses-view.vue';
-import SelesaiMitra from '../pages/selesai-view.vue';
-import SelesaiDitolak from '../pages/selesai-ditolak.vue';
+import MasukManager from '../pages/masuk/masuk-manager.vue';
+import DetailPengajuanManager from '../pages/detail-pengajuan/detail-pengajuan-manager.vue';
+import MasukStaff from '@/pages/masuk/masuk-staff.vue';
+import DetailPengajuanStaff from '@/pages/detail-pengajuan/detail-pengajuan-staff.vue';
+import ApprovalMitra from '@/pages/approval/approval-view.vue';
+import ApprovalStopclock from '@/pages/approval/approval-stopclock.vue';
+import DetailPengajuanApproval from '@/pages/approval/detail-pengajuan-approval.vue';
+import DetailPengajuanApprovalStopclock from '@/pages/approval/detail-pengajuan-approval-stopclock.vue';
+import ProsesMitra from '@/pages/proses/proses-view.vue';
+import SelesaiMitra from '@/pages/selesai/selesai-view.vue';
+import SelesaiDitolak from '@/pages/selesai/selesai-ditolak.vue';
+import DetailPengajuanPKS from '@/pages/proses/proses-detail-pengajuan-pks.vue';
 
 const routes = [
+  {
+    name: 'LoginPage',
+    path: '/login',
+    component: LoginPage,
+  },
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    name: 'Homepage',
+    path: '/homepage',
+    component: HomePage,
+  },
   {
     name: 'Dashboard',
     path: '/dashboard',
@@ -25,6 +44,21 @@ const routes = [
     name: 'DetailPengajuanManager',
     path: '/masukmanager/detailpengajuanmanager',
     component: DetailPengajuanManager
+  },
+  {
+    name: 'MasukStaff',
+    path: '/masukstaff',
+    component: MasukStaff
+  },
+  {
+    name: 'DetailPengajuanStaff',
+    path: '/masukstaff/detailpengajuanstaff',
+    component: DetailPengajuanStaff
+  },
+  {
+    name: 'ApprovalMitra',
+    path: '/approval',
+    component: ApprovalMitra
   },
   {
     name: 'ApprovalMitra',
@@ -47,9 +81,9 @@ const routes = [
     component: DetailPengajuanApprovalStopclock
   },
   {
-    name: 'ProsesiMitra',
+    name: 'ProsesMitra',
     path: '/proses',
-    component: ProsesiMitra
+    component: ProsesMitra
   },
   {
     name: 'SelesaiMitra',
@@ -60,8 +94,12 @@ const routes = [
     name: 'Ditolak',
     path: '/selesai/ditolak',
     component: SelesaiDitolak
-  }
-
+  },
+  {
+    name: 'DetailPengajuanPKS',
+    path: '/proses/detailpengajuanpks',
+    component: DetailPengajuanPKS
+  },
 ];
 
 function Router() {
@@ -71,4 +109,10 @@ function Router() {
   });
 }
 const router = Router();
+
+router.beforeEach((to, from, next) => {
+  document.title = 'Diamond';
+  next();
+});
+
 export default router;

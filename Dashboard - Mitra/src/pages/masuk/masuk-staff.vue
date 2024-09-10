a0c2b1186c75fa6142b7f0e9a51def4dc2dd17c9
Ivan Christian <120652762+christvann@users.noreply.github.com>
2024-09-10 11:21:20 +0700

    Add files via upload

---
+++ Dashboard - Mitra/src/pages/masuk/masuk-staff.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+    <div class="background">
+      <div class="flex">
+        <SidebarStaff />
+        <div class="flex-grow">
+          <MainBar />
+          <MasukStaff />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MainBar from "@/components/MainBar.vue";
+import SidebarStaff from "@/components/SidebarStaff.vue";
+import MasukStaff from "@/components/MasukStaff.vue";
+
+export default {
+  components: {
+    SidebarStaff,
+    MainBar,
+    MasukStaff,
+  },
+};
+</script>
+
+<style>
+.background {
+  width: Fixed (1, 366px) px;
+  height: Hug (1, 416px) px;
+  left: 3733px;
+  gap: 0px;
+  opacity: 0px;
+  background: #f6f7fd;
+}
+</style>

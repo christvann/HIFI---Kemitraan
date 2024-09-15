b2a452effbed9c13595172f97ee8fdc2966d20a8
Ivan Christian <120652762+christvann@users.noreply.github.com>
2024-09-15 12:10:39 +0700

    Add files via upload

---
+++ Dashboard - Mitra/src/pages/selesai/selesai-staff.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="">
+      <div class="background">
+        <div class="flex">
+          <SidebarStaff />
+          <div class="flex-grow">
+            <MainBar />
+            <SelesaiStaff />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import MainBar from "@/components/MainBar.vue";
+  import SidebarStaff from "@/components/SidebarStaff.vue";
+  import SelesaiStaff from "@/components/SelesaiStaff.vue";
+  
+  export default {
+    components: {
+      SidebarStaff,
+      MainBar,
+      SelesaiStaff,
+    },
+  };
+  </script>
+  
+  <style>
+  .background {
+    width: Fixed (1, 366px) px;
+    height: Hug (1, 416px) px;
+    left: 3733px;
+    gap: 0px;
+    opacity: 0px;
+    background: #f6f7fd;
+  }
+  </style>
+  

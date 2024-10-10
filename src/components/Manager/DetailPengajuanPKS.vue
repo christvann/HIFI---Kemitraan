5de21e2cdc993b76536fd02f1b2b20c2cf7d20e4
Ivan Christian <ivanvan0562@gmail.com>
2024-10-10 14:19:20 +0700

    update

--- src/components/Manager/DetailPengajuanPKS.vue
+++ src/components/Manager/DetailPengajuanPKS.vue
@@ -1193,11 +1193,11 @@
         </div>
       </div>
       <div v-if="showDisetujuiPopup" class="fixed inset-0 flex items-center justify-center bg-[#1F2937] bg-opacity-50">
-        <div class="bg-[#FFFFFF] rounded-lg shadow-lg w-[360px] h-[452px]">
+        <div class="bg-[#FFFFFF] rounded-lg shadow-lg w-[360px] h-[415px]">
           <div class="flex flex-col items-center mt-14 ml-8 mr-8 mb-14">
             <img src="@/assets/image/pengajuanditerima.png" class="w-[200px] h-[160px] mb-4" />
             <h2 class="text-[20px] font-sans font-semibold text-[#333333] mb-2">Progress Kemitraan Selesai</h2>
-            <p class="text-[16px] font-sans text-[#333333] font-normal mb-6 text-center">Progress kemitraan PKS telah selesai. Selanjutnya akan dilakukan proses approval oleh atasan.</p>
+            <p class="text-[16px] font-sans text-[#333333] font-normal mb-6 text-center">Progress kemitraan PKS telah selesai.</p>
             <button @click="closeDisetujuiPopup" class="bg-[#2671D9] hover:bg-[#1E5BB7] text-[#FFFFFF] text-[14px] p-2 rounded-lg w-full">Selesai</button>
           </div>
         </div>
